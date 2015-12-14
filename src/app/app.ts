1dbe3ddfb446a0a3ea0975db207d02bfec16999b
swcho <sungwoo.cho.dev@gmail.com>
2015-12-14 20:57:58 +0900

    Initial app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 
-import {bootstrap, Component} from 'angular2/angular2';
+import {Component, View, bootstrap} from 'angular2/angular2';
 
 @Component({
 	selector: 'my-app',
 	template: '<h1>My First Angular 2 App</h1>'
 })
 class AppComponent {
-	
+	public title = 'Tour of Heroes';
+	public hero = 'Windstorm';
 }
 
 bootstrap(AppComponent);
